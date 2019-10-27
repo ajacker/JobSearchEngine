7d18546d9006e361f99137d0e98c2efbcd89885a
ajacker <ajacker@foxmail.com>
2019-10-27 11:45:18 +0800

    初始化commit

---
+++ src/main/java/com/ajacker/searchengine/SearchEngineApplication.java
@@ -0,0 +1,16 @@
+package com.ajacker.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ajacker
+ */
+@SpringBootApplication
+public class SearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineApplication.class, args);
+    }
+
+}
