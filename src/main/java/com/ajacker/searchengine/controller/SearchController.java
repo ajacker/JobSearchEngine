65a37bef43f42c598f66f8d25a99ca3b1f7c256e
ajacker <ajacker@foxmail.com>
2019-10-31 23:41:07 +0800

    完成了搜索条件包装类的编写 前端发送的搜索条件（json）可以正常解析了

--- src/main/java/com/ajacker/searchengine/controller/SearchController.java
+++ src/main/java/com/ajacker/searchengine/controller/SearchController.java
@@ -1,7 +1,9 @@
 package com.ajacker.searchengine.controller;
 
+import com.ajacker.searchengine.pojo.SearchParams;
 import com.ajacker.searchengine.service.impl.JobInfoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +17,8 @@ public class SearchController {
     private JobInfoServiceImpl jobInfoService;
 
     @RequestMapping("/search")
-    public void search(String keyword,String salary,String time,String education,String exp,String place){
-        System.out.println(keyword);
-        System.out.println(salary);
-        System.out.println(time);
-        System.out.println(education);
-        System.out.println(exp);
-        System.out.println(place);
-        jobInfoService.search(keyword, salary, time, education, exp, place);
+    public void search(@RequestBody SearchParams params) {
+        System.out.println(params);
 
     }
 }
---
+++ src/main/java/com/ajacker/searchengine/pojo/SearchParams.java
@@ -0,0 +1,48 @@
+package com.ajacker.searchengine.pojo;
+
+import lombok.Data;
+
+/**
+ * @author ajacker
+ * @date 2019/10/31 23:32
+ */
+@Data
+public class SearchParams {
+    /**
+     * 搜索关键字
+     */
+    private String keyword;
+    /**
+     * 工作地点
+     */
+    private String place;
+    /**
+     * 工作经验的要求
+     */
+    private String exp;
+    /**
+     * 薪资要求（k）
+     */
+    private String salary;
+    /**
+     * 最近time天
+     */
+    private int time;
+    /**
+     * TODO:学历最小要求
+     */
+    private int education;
+    /**
+     * 当前页码
+     */
+    private int pageNumber;
+    /**
+     * 一页几个
+     */
+    private int pageSize;
+    /**
+     * 排序方式asc/
+     */
+    private String sortOrder;
+
+}
